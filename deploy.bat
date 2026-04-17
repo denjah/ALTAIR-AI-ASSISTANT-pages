f0cd6003a66c2644956e077a63ca35ab6680b0f6
ALTAIR Senior Analyst AI <senior-analyst@altaircom.ru>
2026-04-17 16:50:38 +0300

    cleanup: remove redundant root html file

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+mkdir azimut-43504
+copy "z:\!_STUFF\ALTAIR\assistant-project\design\43504\index.html" "azimut-43504\index.html" /Y
+copy "z:\!_STUFF\ALTAIR\assistant-project\ALTAIR-AI-ASSISTANT-pages\assets\altair_logo.svg" "azimut-43504\altair_logo.svg" /Y
+copy "z:\!_STUFF\ALTAIR\assistant-project\ALTAIR-AI-ASSISTANT-pages\EuropeExt-Bold.otf" "azimut-43504\EuropeExt-Bold.otf" /Y
+git add azimut-43504
+git commit -m "Deploy AZIMUT 43504 deep analysis report"
+git push
